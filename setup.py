ceef4cbaeca6720615f0e92bfc8f2958a56d9bec
Charles Inwald <cci219@lehigh.edu>
2019-11-16 22:38:34 -0500

    Finished backend I think

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='redditsplit',
+    version='',
+    packages=['praw', 'nltk', 'pandas', 'gensim', 'flask', 'sklearn', 'flask-cors', 'spacy', ],
+    url='',
+    license='',
+    author='Charles Inwald',
+    author_email='',
+    description=''
+)
